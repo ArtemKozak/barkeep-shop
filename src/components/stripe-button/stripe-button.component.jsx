b96c68e24e32a7111b89cc58e9b0340405396df0
Druid72 <timon4k37@gmail.com>
2020-03-22 03:00:39 +0200

    added stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({price}) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_whmf9hUx2SKbyrlITOIkS5aZ00zmWd2t8g';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    };
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='Barkeep Shop'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+};
+
+export default StripeCheckoutButton;
