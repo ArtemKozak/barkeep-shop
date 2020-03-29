942bfeb7dd41b93f18fabb0bbaa1f0d7ff92c0ed
Druid72 <timon4k37@gmail.com>
2020-03-29 16:35:41 +0300

    added loading spinner and moved shop data to firestore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,51 +1,83 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyDZV38RO1Led2MAt7cl7Yi0yJQIT1pxAmA",
-    authDomain: "barkeep-shop.firebaseapp.com",
-    databaseURL: "https://barkeep-shop.firebaseio.com",
-    projectId: "barkeep-shop",
-    storageBucket: "barkeep-shop.appspot.com",
-    messagingSenderId: "351259110076",
-    appId: "1:351259110076:web:0102838b70d4bac549feb9",
-    measurementId: "G-X5MLSJ6SJS"
-};
-
-export const createUserProfileDocument = async (userAuth, additionalData) => {
-    if (!userAuth) return;
-
-    const userRef = firestore.doc(`users/${userAuth.uid}`);
-
-    const snapShot = await userRef.get();
-
-    if (!snapShot.exists) {
-        const { displayName, email } = userAuth;
-        const createAt = new Date();
-
-        try {
-            await userRef.set({
-                displayName,
-                email,
-                createAt,
-                ...additionalData
-            })
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    return userRef;
+  apiKey: "AIzaSyDZV38RO1Led2MAt7cl7Yi0yJQIT1pxAmA",
+  authDomain: "barkeep-shop.firebaseapp.com",
+  databaseURL: "https://barkeep-shop.firebaseio.com",
+  projectId: "barkeep-shop",
+  storageBucket: "barkeep-shop.appspot.com",
+  messagingSenderId: "351259110076",
+  appId: "1:351259110076:web:0102838b70d4bac549feb9",
+  measurementId: "G-X5MLSJ6SJS"
 };
 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account'});
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
