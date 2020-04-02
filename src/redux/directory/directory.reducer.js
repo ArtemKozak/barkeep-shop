3c7901d0a65fa0c1a70464f882cf31c255400cca
Druid72 <timon4k37@gmail.com>
2020-04-02 17:43:31 +0300

    fixed spinner and added bar collections

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -1,54 +1,55 @@
 const INITIAL_STATE = {
   sections: [
-    {
-      title: 'wineCorks',
-      imageUrl: 'https://i.ibb.co/sQRbSxr/wineCork.jpg',
-      id: 1,
-      linkUrl: 'shop/hats'
-    },
+    // {
+    //   title: 'wineCorks',
+    //   imageUrl: 'https://i.ibb.co/sQRbSxr/wineCork.jpg',
+    //   id: 1,
+    //   linkUrl: 'shop/hats'
+    // },
     {
       title: 'jiggers',
       imageUrl: 'https://i.ibb.co/6H0MYKm/jigger.jpg',
       id: 2,
-      linkUrl: 'shop/hats'
+      linkUrl: 'shop/jiggers'
     },
-    {
-      title: 'shakers',
-      imageUrl: 'https://i.ibb.co/kG3XcQS/shaker.jpg',
-      id: 3,
-      linkUrl: 'shop/hats'
-    },
+    // {
+    //   title: 'shakers',
+    //   imageUrl: 'https://i.ibb.co/kG3XcQS/shaker.jpg',
+    //   id: 3,
+    //   linkUrl: 'shop/hats'
+    // },
     {
       title: 'shakersBostonsky',
       imageUrl: 'https://i.ibb.co/xfYQ5bb/shaker-Bostonsky.jpg',
       id: 4,
-      linkUrl: 'shop/hats'
+      linkUrl: 'shop/shakersbostonsky'
     },
     {
       title: 'strainers',
       imageUrl: 'https://i.ibb.co/605XVJ7/strainer.jpg',
       id: 5,
-      linkUrl: 'shop/hats'
+      linkUrl: 'shop/strainers'
     },
     {
       title: 'barSpoons',
       imageUrl: 'https://i.ibb.co/fdfh9nP/baeSpoon.jpg',
+      size: 'large',
       id: 6,
-      linkUrl: 'shop/hats'
+      linkUrl: 'shop/barspoons'
     },
     {
       title: 'geysers',
       imageUrl: 'https://i.ibb.co/wzFcVV5/geyser.jpg',
       size: 'large',
       id: 7,
-      linkUrl: 'shop/hats'
+      linkUrl: 'shop/geysers'
     },
     {
       title: 'madlers',
       imageUrl: 'https://i.ibb.co/3pXHmqq/madler.jpg',
       size: 'large',
       id: 8,
-      linkUrl: 'shop/hats'
+      linkUrl: 'shop/madlers'
     }
   ]
 };
