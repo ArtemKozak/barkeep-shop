66794c7d553aa98c40d623c4a1237e6c6583f6c2
Druid72 <timon4k37@gmail.com>
2020-04-08 15:21:45 +0300

    added mobile view for cart dropdown

--- client/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ client/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,7 +3,8 @@ import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
 import {withRouter} from 'react-router-dom';
 
-import CartItem from '../cart-item/cart-item.component';
+// import CartItem from '../cart-item/cart-item.component';
+import CheckoutItem from "../checkout-item/checkout-item.component";
 import {selectCartItems} from '../../redux/cart/cart.selectors';
 import {toggleCartHidden} from '../../redux/cart/cart.actions.js';
 
@@ -11,29 +12,36 @@ import {
     CartDropdownButton,
     CartDropdownContainer,
     CartItemsContainer,
-    EmptyMessageContainer
+    EmptyMessageContainer,
+    FullScreenDropdownContainer,
+    CloseCartDropdownContainer,
+    CloseCardTextContainer
 } from './cart-dropdown.styles';
 
 const CartDropdown = ({cartItems, history, dispatch}) => (
-    <CartDropdownContainer>
-        <CartItemsContainer>
-            {cartItems.length ? (
-                cartItems.map(cartItem => (
-                    <CartItem key={cartItem.id} item={cartItem}/>
-                ))
-            ) : (
-                <EmptyMessageContainer>Your cart is empty</EmptyMessageContainer>
-            )}
-        </CartItemsContainer>
-        <CartDropdownButton
-            onClick={() => {
-                history.push('/checkout');
-                dispatch(toggleCartHidden());
-            }}
-        >
-            GO TO CHECKOUT
-        </CartDropdownButton>
-    </CartDropdownContainer>
+    <FullScreenDropdownContainer>
+        <CloseCartDropdownContainer onClick={() => {dispatch(toggleCartHidden())}}/>
+        <CartDropdownContainer>
+            <CloseCardTextContainer onClick={() => {dispatch(toggleCartHidden())}}>Закрыть</CloseCardTextContainer>
+            <CartItemsContainer>
+                {cartItems.length ? (
+                    cartItems.map(cartItem => (
+                        <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
+                    ))
+                ) : (
+                    <EmptyMessageContainer>Корзина пустая</EmptyMessageContainer>
+                )}
+            </CartItemsContainer>
+            <CartDropdownButton
+                onClick={() => {
+                    history.push('/checkout');
+                    dispatch(toggleCartHidden());
+                }}
+            >
+                Оформить Заказ
+            </CartDropdownButton>
+        </CartDropdownContainer>
+    </FullScreenDropdownContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
--- client/src/components/cart-dropdown/cart-dropdown.styles.jsx
+++ client/src/components/cart-dropdown/cart-dropdown.styles.jsx
@@ -1,22 +1,46 @@
 import styled from 'styled-components';
 import CustomButton from '../custom-button/custom-button.component';
 
+export const FullScreenDropdownContainer = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  display: flex;
+  z-index: 5;
+`;
+
+export const CloseCartDropdownContainer = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  opacity: 0.15;
+  background-color: black;
+  z-index: 5;
+`;
+
 export const CartDropdownContainer = styled.div`
-  position: absolute;
-  width: 240px;
-  height: 340px;
+  margin: auto;
+  width: 40%;
+  height: 70%;
   display: flex;
   flex-direction: column;
   padding: 20px;
   border: 1px solid black;
+  border-radius: 5px;
   background-color: white;
-  top: 90px;
-  right: 40px;
-  z-index: 5;
+  z-index: 6;
+  
+  @media screen and (max-width: 800px) {
+    width: 92%;
+    height: 90%;
+  }
 `;
 
 export const CartDropdownButton = styled(CustomButton)`
   margin-top: auto;
+  height: 60px;
 `;
 
 export const EmptyMessageContainer = styled.span`
@@ -24,8 +48,24 @@ export const EmptyMessageContainer = styled.span`
   margin: 50px auto;
 `;
 
+export const CloseCardTextContainer = styled.span`
+  visibility: hidden;
+  height: 0;
+  font-size: 14px;
+  margin-left: auto;
+  
+  
+  @media screen and (max-width: 800px) {
+    visibility: visible;
+    height: unset;
+    cursor: pointer;
+    margin-bottom: 10px;
+  }
+`;
+
 export const CartItemsContainer = styled.div`
-  height: 240px;
+  height: 100%;
+  margin-bottom: 20px;
   
   flex-direction: column;
   overflow: scroll;
