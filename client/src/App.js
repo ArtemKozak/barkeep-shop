bfb421f46fe984f49fe73563a40098ac2534ab15
Druid72 <timon4k37@gmail.com>
2020-04-08 19:15:11 +0300

    added terms of use

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
+import TermOfUsePage from "./pages/terms-of-use/terms-of-use.component";
 
 import Header from './components/header/header.component';
 
@@ -36,6 +37,7 @@ class App extends React.Component {
                     <Route exact path='/' component={HomePage}/>
                     <Route path='/shop' component={ShopPage}/>
                     <Route exact path='/checkout' component={CheckoutPage}/>
+                    <Route exact path='/terms' component={TermOfUsePage}/>
                     <Route
                         exact
                         path='/signin'
