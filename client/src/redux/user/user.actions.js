632730511a8a8cab4b3fb4c08f6a93a643ee6a76
Druid72 <timon4k37@gmail.com>
2020-04-10 16:31:55 +0300

    small reducer changes

--- client/src/redux/user/user.actions.js
+++ client/src/redux/user/user.actions.js
@@ -1,12 +1,5 @@
 import UserActionTypes from './user.types';
 
-export const googleSignInStart = () => ({
-    type: UserActionTypes.GOOGLE_SIGN_IN_START
-});
-
-export const facebookSignInStart = () => ({
-    type: UserActionTypes.FACEBOOK_SIGN_IN_START
-});
 
 export const signInSuccess = user => ({
     type: UserActionTypes.SIGN_IN_SUCCESS,
@@ -16,6 +9,14 @@ export const signInSuccess = user => ({
 export const signInFailure = error => ({
     type: UserActionTypes.SIGN_IN_FAILURE,
     payload: error
+});
+
+export const googleSignInStart = () => ({
+    type: UserActionTypes.GOOGLE_SIGN_IN_START
+});
+
+export const facebookSignInStart = () => ({
+    type: UserActionTypes.FACEBOOK_SIGN_IN_START
 });
 
 export const emailSignInStart = emailAndPassword => ({
