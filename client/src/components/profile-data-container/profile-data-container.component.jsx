7dbaae681cb06c37f50ece1e03ae1c59e41a13d3
Druid72 <timon4k37@gmail.com>
2020-04-10 19:11:15 +0300

    new props for button

--- client/src/components/profile-data-container/profile-data-container.component.jsx
+++ client/src/components/profile-data-container/profile-data-container.component.jsx
@@ -3,31 +3,32 @@ import shortid from 'shortid';
 
 import ProfileAddDataButton from "../profile-add-data-button/profile-add-data-button.component";
 
-import {
-    TextContainer,
-    LabelContainer,
-    FlexTextsContainer,
-    UserDataContainer
-} from './profile-data-container.styles';
+import {FlexTextsContainer, LabelContainer, TextContainer, UserDataContainer} from './profile-data-container.styles';
 
 const UserData = (props) => (
-        <UserDataContainer>
-            <LabelContainer>{props.label}</LabelContainer>
-            <FlexTextsContainer>
-                {console.log(props.otherText)}
-                {
-                    !props.otherText ? (
-                        <TextContainer as='div'>
-                            <ProfileAddDataButton />
-                        </TextContainer>
-                    ) : (
-                        props.otherText.map((text) => (
-                            <TextContainer key={shortid.generate()}>{text}</TextContainer>
-                        ))
-                    )
-                }
-            </FlexTextsContainer>
-        </UserDataContainer>
+    <UserDataContainer>
+        <LabelContainer>{props.label}</LabelContainer>
+        <FlexTextsContainer>
+            {
+                !props.otherText ? (
+                    <TextContainer as='div'>
+                        <ProfileAddDataButton/>
+                    </TextContainer>
+                ) : (
+                    props.otherText.map((text) => (
+                        <TextContainer key={shortid.generate()}>{text}</TextContainer>
+                    ))
+                )
+            }
+            {
+                props.canAdd ? (
+                    <TextContainer as='div'>
+                        <ProfileAddDataButton/>
+                    </TextContainer>
+                ) : null
+            }
+        </FlexTextsContainer>
+    </UserDataContainer>
 );
 
 export default UserData;
--- client/src/components/profile-personal-information/profile-personal-information.component.jsx
+++ client/src/components/profile-personal-information/profile-personal-information.component.jsx
@@ -9,11 +9,16 @@ const PersonalInformation = ({currentUser}) => (
     <PersonalInformationContainer>
         <TitleContainer>Личные данные</TitleContainer>
         <UserData label={"Имя"} otherText={[currentUser.displayName]}/>
-        <UserData label={"Электронная потча"} otherText={null}/>
-        <UserData label={"Телефоны"}
-                  otherText={[formatPhoneNumberIntl("+380990123456"), formatPhoneNumberIntl("+380997891011")]}/>
-        <UserData label={"Адреса для доставок"}
-                  otherText={["Киев, НАЗВАНИЕ2 УЛ., д.2/5 кв.45", "Киев, НАЗВАНИЕ1 УЛ., д.4 кв.52"]}/>
+        <UserData label={"Электронная потча"} otherText={[currentUser.email]}/>
+        <UserData
+            label={"Телефоны"}
+            otherText={[formatPhoneNumberIntl("+380990123456"), formatPhoneNumberIntl("+380997891011")]}
+            canAdd={true}/>
+        <UserData
+            label={"Адреса для доставок"}
+            otherText={["Киев, НАЗВАНИЕ2 УЛ., д.2/5 кв.45", "Киев, НАЗВАНИЕ1 УЛ., д.4 кв.52"]}
+            canAdd={true}
+        />
     </PersonalInformationContainer>
 );
 
